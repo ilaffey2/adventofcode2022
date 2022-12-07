676eac748be2e065a7fd48099631e6853bae9934
ian <ian.laffey@mail.mcgill.ca>
2022-12-07 00:20:32 -0700

    day07

---
+++ day07/part1.py
@@ -0,0 +1,50 @@
+from treelib import Node, Tree
+file = open('input.txt', 'r')
+lines = file.readlines()
+
+tree = Tree()
+tree.create_node("/", "/")
+pwd = "/"
+ls_mode = False
+for line in lines[1:]:
+    line = line.strip()
+    argv = line.split(" ")
+    if argv[0] == "$":
+        ls_mode = False
+        if argv[1] == "cd":
+            if argv[2] == "..":
+                pwd = pwd.split("/")[:-2]
+                print(pwd)
+                pwd = "/".join(pwd) + "/"
+                print(pwd)
+
+            else:
+                pwd = pwd + argv[2] + "/"
+        if argv[1] == 'ls':
+            ls_mode = True
+    else:
+        if argv[0] == "dir":
+                print(argv)
+                tree.create_node(argv[1], pwd + argv[1] + "/", parent=pwd)
+        else:
+                tree.create_node(argv[1], pwd + argv[1] + "/", parent=pwd, data=argv[0])
+            
+result_list = []
+def calc_size(node):
+    size = 0
+    children = tree.children(node)
+    if len(children) > 0:
+        for child in children:
+            size += calc_size(child.identifier)
+    else:
+        if tree.get_node(node).data != None:
+            size = int(tree.get_node(node).data)
+        else:
+            size = 0
+    if size <= 100000 and len(children) > 0:
+        result_list.append(size)
+    return size 
+
+print(tree.show())
+print(calc_size(('/')))
+print(sum(result_list))
